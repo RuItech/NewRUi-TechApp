71eff03dfdeb61df37913d4959404c4be89fd6ad
ZaloAustine <austinezaloa@gmail.com>
2019-11-07 18:22:45 -0500

    New ru-itech app

---
+++ app/src/main/java/com/example/ru_itech/CommonUI/Login.java
@@ -0,0 +1,16 @@
+package com.example.ru_itech.CommonUI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ru_itech.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/example/ru_itech/CommonUI/SplashScreen.java
@@ -0,0 +1,41 @@
+package com.example.ru_itech.CommonUI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.ru_itech.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //splash screen
+        Splash splash = new Splash();
+        splash.start();
+
+    }
+
+    //splash screen thread
+    public class Splash extends Thread{
+        @Override
+        public void run() {
+            super.run();
+
+            try {
+                sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            //intent to signUp screen
+            startActivity(new Intent(SplashScreen.this,SignUp.class));
+            finish();
+
+        }
+    }
+}
