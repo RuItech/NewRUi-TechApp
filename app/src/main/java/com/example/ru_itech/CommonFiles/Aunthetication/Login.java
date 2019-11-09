3d143bd2dc5dd4aaa061e2b3018126e7b6b51485
ZaloAustine <austinezaloa@gmail.com>
2019-11-09 13:34:25 -0500

    Login and Signup Design redesign

--- app/src/main/java/com/example/ru_itech/CommonFiles/Aunthetication/Login.java
+++ app/src/main/java/com/example/ru_itech/CommonFiles/Aunthetication/Login.java
@@ -2,8 +2,12 @@ package com.example.ru_itech.CommonFiles.Aunthetication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.example.ru_itech.CommonFiles.UI.CommonNewsPage;
 import com.example.ru_itech.R;
 
 public class Login extends AppCompatActivity {
@@ -12,5 +16,22 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        Button login = findViewById(R.id.Login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               startActivity(new Intent(Login.this, DashBoard.class));
+            }
+        });
+
+        Button signup = findViewById(R.id.goSignup);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Login.this, SignUp.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/ru_itech/CommonFiles/Aunthetication/SignUp.java
+++ app/src/main/java/com/example/ru_itech/CommonFiles/Aunthetication/SignUp.java
@@ -2,7 +2,10 @@ package com.example.ru_itech.CommonFiles.Aunthetication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.ru_itech.R;
 
@@ -12,5 +15,13 @@ public class SignUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        Button gologin = findViewById(R.id.gologin);
+        gologin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SignUp.this,Login.class));
+            }
+        });
     }
 }
